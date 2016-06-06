4b8df364a80dafd9609a75f35cc094b7d2f97040
Felix <felix@playgen.com>
2016-06-06 16:55:01 +0100

    clean up code

--- Assets/SatisfactionSlider.cs
+++ Assets/SatisfactionSlider.cs
@@ -11,14 +11,23 @@ public class SatisfactionSlider : MonoBehaviour {
     private RectTransform mySlider;
 	// Use this for initialization
 	void Start () {
+        Setup();
+	}
+
+    private void Setup()
+    {
         myRect = this.gameObject.GetComponent<RectTransform>();
         mySlider = slider.GetComponent<RectTransform>();
 
         initColor = warningBG.color;
-	}
+    }
 
     public void SetSlider(float happiness)
     {
+        if (mySlider == null || myRect == null)
+        {
+            Setup();
+        }
         mySlider.anchoredPosition = new Vector2(myRect.rect.width - ((myRect.rect.width / 100f) * happiness), 0f);
         warningBG.color = new Color(initColor.r, initColor.g, initColor.b, 1f - (happiness / 100f));
     }
--- Assets/Scripts/SimplifiedJson.cs
+++ Assets/Scripts/SimplifiedJson.cs
@@ -4,6 +4,18 @@ using SimpleJSON;
 
 public class SimplifiedJson : MonoBehaviour {
 
+    //how the json data is stored
+    /*
+     * each scenario is broken down into different incidents each with their own index
+     * 
+     * [_INDEX_] : [ [_TYPE_] , [_NAME_] , [_WAIT_INDEX_] , [_OFFICER_INDEX_] , [_CITIZEN_INDEX] , [_OFFICERS_] , [_TURNS_REQUIRED_] , [_SEVERITY_] , [_SATISFACTION_IMPACT] ]
+     * 
+     * for easy look up the idexes are below
+     * 
+     * [_INDEX_] : [ type=0, name=1, waitIndex=2. officerIndex=3, citizenIndex=4, officers=5, turnsRequired=6, severity=7, satisfactionImpact=8 ]
+     * 
+     */ 
+
     string filePath = "ScenarioInformation";
     TextAsset myText;
     TurnManager manager;
@@ -29,20 +41,6 @@ public class SimplifiedJson : MonoBehaviour {
         //add the case number to help the player keep track of it throughout its life
         zIncident.caseNumber = identifier;
         identifier++;
-
-        ////get a random incident
-        //int randIncident = UnityEngine.Random.Range(0, int.Parse(N["TotalIncidentTypes"]));
-        //int randArea = UnityEngine.Random.Range(0, int.Parse(N["TotalAreaTypes"]));
-
-        //zIncident.index = randIncident;
-        //zIncident.officer = int.Parse(N["Incidents"][randIncident][1]);
-        //zIncident.area = N["Areas"][randArea];
-        //zIncident.incidentName = GetIncidentFromType(N["Incidents"][randIncident][0]);
-        //zIncident.turnsToAdd = int.Parse(N["Incidents"][randIncident][3]);
-        //zIncident.resolved = false;
-        //zIncident.caseNumber = identifier;
-        //zIncident.severity = int.Parse(N["Incidents"][randIncident][4]);
-        //identifier++;
     }
     private Incident GetIncidentAtIndex(int index, int scenarioNum, int caseNum = 0)
     {
@@ -114,73 +112,6 @@ public class SimplifiedJson : MonoBehaviour {
         zIncident = GetIncidentAtIndex(index, zIncident.scenarioNum, zIncident.caseNumber);
         zIncident.turnToShow = manager.turn + 1;
     }
-    //public void DevelopIncident(ref Incident zIncident, bool waiting)
-    //{
-    //    //we must now see if the incident can be developed
-    //    if (myText == null)
-    //        myText = Resources.Load(filePath) as TextAsset;
-    //    var N = JSON.Parse(myText.text);
-
-    //    //check the index of the developed index
-    //    int index = int.Parse(N["Incidents"][zIncident.index][2]);
-
-    //    if (index == -1)
-    //    {
-    //        TurnManager turn = GameObject.Find("TurnManager").GetComponent<TurnManager>();
-    //        if (turn.turn >= zIncident.turnToDevelop)
-    //        {
-    //            //the incident that does not develop will expire, for now we will resolve the case, TODO make this a unsuccessful resolution
-    //            zIncident.resolved = true;
-    //            zIncident.expired = true;
-    //            zIncident.positiveResolution = false;
-    //        }
-    //        else if (waiting)
-    //        {
-    //            //case does not resolve if you do nothing and has no developments, instead we will delay the showing of this message
-    //            zIncident.turnToShow += 1;
-    //        }
-    //        else
-    //        {
-    //            //we must resolve the issue
-    //            zIncident.resolved = true;
-    //            zIncident.positiveResolution = true;
-    //            zIncident.turnToShow += zIncident.turnsToAdd;
-    //        }
-    //    }
-    //    else
-    //    {
-    //        if (waiting)
-    //        {
-    //            //check to see if the current incident has been ignored enough to develop into a new incident
-    //            TurnManager turn = GameObject.Find("TurnManager").GetComponent<TurnManager>();
-    //            if (zIncident.turnToDevelop >= turn.turn-1)
-    //            {
-    //                zIncident.turnToShow += 1;
-    //            }
-    //            else
-    //            {
-    //                //bad response so develop the situation
-    //                zIncident.nameBeforeDeveloped = zIncident.incidentName;
-    //                zIncident.incidentName = GetIncidentFromType(N["Incidents"][index][0]);
-    //                zIncident.officer = int.Parse(N["Incidents"][index][1]);
-    //                zIncident.turnToShow += 1;
-    //                zIncident.turnsToAdd = int.Parse(N["Incidents"][index][3]);
-    //                zIncident.turnToDevelop = turn.turn + 3;
-    //                zIncident.severity = int.Parse(N["Incidents"][index][4]);
-    //                zIncident.index = index;
-    //                zIncident.developed = true;
-    //            }
-
-    //        }
-    //        else
-    //        {
-    //            //good response so dont branch
-    //            zIncident.resolved = true;
-    //            zIncident.positiveResolution = true;
-    //            zIncident.turnToShow += zIncident.turnsToAdd;
-    //        }
-    //    }
-    //}
     public string GetIncidentFromType(string Type)
     {
         string description = "";
