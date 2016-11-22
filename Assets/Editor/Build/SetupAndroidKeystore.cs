3b5d0e680ca917c6419f51c70abfeab746ff4575
Felix <felix@playgen.com>
2016-11-22 12:14:31 +0000

    Add gamework code for jenkins build Update player settings version number

---
+++ Assets/Editor/Build/SetupAndroidKeystore.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using GameWork.Unity.Editor.Build;
+using UnityEditor;
+using UnityEngine;
+using EventType = GameWork.Unity.Editor.Build.EventType;
+
+namespace Editor.Build
+{
+    public static class SetupAndrodKeystore
+    {
+        [MenuItem("Tools/Build/Android/Setup Keystore")]
+        [BuildEvent(EventType.Pre)]
+        public static void Setup()
+        {
+            PlayerSettings.Android.keystoreName = Application.dataPath + "/rfkeystore.keystore";
+            PlayerSettings.Android.keystorePass = "kpsLsTorAygEeN";
+
+            PlayerSettings.Android.keyaliasName = "releasebuildrf";
+            PlayerSettings.Android.keyaliasPass = "kpsLsTorAygEeN";
+        }
+    }
+}
