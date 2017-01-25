39cb6e6f7497445f92634cc7b6fda22a2443d702
Felix <felix@playgen.com>
2017-01-25 16:18:14 +0000

    make sure the info panels do not show whilst the feedback panel is showing

--- Assets/Scripts/Incidents/WarningBox.cs
+++ Assets/Scripts/Incidents/WarningBox.cs
@@ -10,7 +10,7 @@ public class WarningBox : MonoBehaviour
     public GameObject ScreenFade;
 
     ///This class will show and hide the warning box when called by the dialogBox class
-    private bool _showingPopup;
+    public bool ShowingPopup { get; private set; }
     private Text _warningText;
     private Image _borderImage;
 
@@ -23,7 +23,7 @@ public class WarningBox : MonoBehaviour
     public IEnumerator ShowWarning(string message, Color color, bool upperCase = false)
     {
         SetColor(color);
-        if (!_showingPopup)
+        if (!ShowingPopup)
         {
             ScreenFade.SetActive(true);
             if (_warningText == null)
@@ -35,7 +35,7 @@ public class WarningBox : MonoBehaviour
 
             AudioManager.Instance.ShowWarningMessage();
 
-            _showingPopup = true;
+            ShowingPopup = true;
 
             var anim = this.GetComponent<Animation>();
 
@@ -51,7 +51,7 @@ public class WarningBox : MonoBehaviour
             anim.Play("WarningPopupExit");
             yield return anim["WarningPopupExit"].clip.length;
 
-            _showingPopup = false;
+            ShowingPopup = false;
             ScreenFade.SetActive(false);
         }
     }
