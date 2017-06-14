22d52d9c34330a8c25b0c19d7db6d2ea632303b3
BAYMAX\Felix <felix@playgen.com>
2017-06-14 11:54:09 +0100

    serialise response from authoriing tool to object to be used in game Disable warning as errors

---
+++ Assets/Scripts/ContentRequest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ContentRequest : MonoBehaviour
+{
+
+    public string HostName;
+    public string Port;
+
+    private int _serialNumber = 10;
+
+    private string _api
+    {
+        get { return "http://" + HostName + ":" + Port + "/api"; }
+    }
+
+    private string _extension
+    {
+        get { return "/scenario/new/" + _serialNumber; }
+    }
+
+    void Start()
+    {
+        StartCoroutine(FetchNewContent());
+    }
+
+    private IEnumerator FetchNewContent()
+    {
+        var www = new WWW(_api + _extension);
+
+        yield return www;
+
+        if (www.text == null)
+        {
+            Debug.Log("Unable to find any new content");
+        }
+
+        // Remove object brackets []
+        var body = www.text.Substring(1, www.text.Length - 2);
+
+        var scenarios = JsonConvert.DeserializeObject<Scenario>(body);
+
+        Debug.Log(scenarios);
+    }
+}
---
+++ Assets/Scripts/Scenario.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Scenario
+{
+    public long SerialNumber;
+    public ScenarioWrapper Content;
+}
+
+public class ScenarioWrapper
+{
+    private string _type = "Content";
+    public IncidentContent Scene;
+}
+
+public class IncidentContent
+{
+    private string _type = "scene";
+    public string Title;
+    public string Description;
+    public int Severity;
+    public int OfficerReq;
+    public int TurnReq;
+    public int SatisfactionImpact;
+    public List<IncidentChoice> Choices;
+}
+
+public class ChoiceFeedback
+{
+    private string _type = "ChoiceAction";
+    public string ChoiceType;
+    public int FeedbackRating;
+    public string Feedback;
+}
+
+public class IncidentChoice
+{
+    private string _type = "Choice";
+    public ChoiceFeedback Choice;
+    public IncidentContent Scene;
+}
