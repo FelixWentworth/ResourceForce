a7f47de43f8fd53f02db8d4913db5b27ac1af608
BAYMAX\Felix <felix@playgen.com>
2017-08-02 17:45:10 +0100

    Add animations for button presses

--- Assets/Scripts/GameFlow/GameOver.cs
+++ Assets/Scripts/GameFlow/GameOver.cs
@@ -67,15 +67,15 @@ public class GameOver : MonoBehaviour
         // We want to display the scores one at a time to make the page more interesting
         yield return new WaitForSeconds(0.5f);
 
-        TurnsLabel.text = Localization.Get(TurnsLabelKey) + ": x" + turns;
+        TurnsLabel.text = turns + " " + Localization.Get(TurnsLabelKey) + ":" ;
         StartCoroutine(IncrementTextNumber(ScoreText, 0, turnsScore));
         yield return IncrementTextNumber(TurnsText, 0, turnsScore);
 
-        GoodCasesLabel.text = Localization.Get(GoodCasesLabelKey) + ": x" + goodCases;
+        GoodCasesLabel.text = goodCases + " " + Localization.Get(GoodCasesLabelKey) + ":";
         StartCoroutine(IncrementTextNumber(ScoreText, turnsScore, (turnsScore + goodCaseScore)));
         yield return IncrementTextNumber(GoodCaseText, 0, goodCaseScore);
 
-        BadCasesLabel.text = Localization.Get(BadCasesLabelKey) + ": x" + badCases;
+        BadCasesLabel.text = badCases + " " + Localization.Get(BadCasesLabelKey) + ":";
         StartCoroutine(IncrementTextNumber(ScoreText, (turnsScore + goodCaseScore), score));
         yield return IncrementTextNumber(BadCaseText, 0, badCaseScore);  
 
