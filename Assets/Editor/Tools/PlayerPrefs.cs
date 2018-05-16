72de89ca512a054cd8136850fdb5dcde9a938ac4
Jared <jaredgglass@gmail.com>
2018-05-16 15:52:38 +0100

    tool to delete player prefs

---
+++ Assets/Editor/Tools/PlayerPrefs.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Editor.Tools
+{
+    public class PlayerPrefs
+    { 
+        [MenuItem("Tools/Player Prefs/Delete All")]
+        public static void DeleteAll()
+        {
+            UnityEngine.PlayerPrefs.DeleteAll();
+        }
+    }
+}
